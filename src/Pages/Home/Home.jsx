314af5c638842b26415f244873d49cd5079a1f0b
Tahsan89 <tahsanislam079@gmail.com>
2024-03-05 00:09:34 +0600

    comments

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+// import Navbar from "../../Components/Navbar/Navbar";
+
+const Home = () => {
+   return (
+      <div>
+         {/* <Navbar></Navbar> */}
+         {/* this is nav */}
+         <h2>This is home.</h2>
+
+      </div>
+   );
+};
+
+export default Home;
