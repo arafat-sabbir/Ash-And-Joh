71eb065878296345fccceb350b87dd59b74bb978
Tahsan89 <tahsanislam079@gmail.com>
2024-03-08 00:52:09 +0600

    this commit is for switching branch

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
-import Banner from "@/components/Banner/Banner";
-import Navbar from "@/components/Navbar/Navbar";
+import Banner from "@/Components/Banner/Banner";
+import Navbar from "@/Components/Navbar/Navbar";
+
 
 const Home = () => {
     return (
