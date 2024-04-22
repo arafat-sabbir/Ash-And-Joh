325e868df0c1e5c66d9966c92a40388f1cddf9d3
arafat-sabbir <arafatshabbir8@gmail.com>
2024-04-22 23:45:30 +0600

    Maded the manage product table centered

--- src/Pages/DashBoard/AdminDashboard/ManageProduct.jsx
+++ src/Pages/DashBoard/AdminDashboard/ManageProduct.jsx
@@ -11,18 +11,18 @@ const ManageProduct = () => {
     const handleDeleteProduct = (id) => {
         toast('Are You Sure Want To Delete', {
             action: {
-              label: 'Yes',
-              button: "NO",
-              onClick: () => {
-                axiosSecure.delete(`/products/deleteProduct/${id}`).then((res) => {
-                  if (res.status === 200) {
-                    toast.success("Product deleted Successfully");
-                    refetch();
-                  }
-                });
-              },
+                label: 'Yes',
+                button: "NO",
+                onClick: () => {
+                    axiosSecure.delete(`/products/deleteProduct/${id}`).then((res) => {
+                        if (res.status === 200) {
+                            toast.success("Product deleted Successfully");
+                            refetch();
+                        }
+                    });
+                },
             },
-          });
+        });
     }
     if (isLoading) {
         const loadingCard = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -35,77 +35,75 @@ const ManageProduct = () => {
     }
 
     return (
-        <div>
-            <div className="h-full w-full flex flex-col justify-center ">
-                <div className="flex flex-col mb-10">
-                    <h1 className="text-3xl font-semibold text-center mb-10 ml-14">
-                        Manage All The Product
-                    </h1>
-                    <div className="flex flex-col lg:container max-w-[98vw] mx-auto">
-                        <div className="overflow-x-auto border-2 rounded-xl">
-                            <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                                <thead>
-                                    <tr>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                                        >
-                                            Product Image
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                                        >
-                                            Product Price
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                                        >
-                                            Product Title
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                                        >
-                                            AvailAble Size
-                                        </th>
-                                        <th
-                                            scope="col"
-                                            className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
-                                        >
-                                            Action
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                                    {allProduct?.map((item) => (
-                                        <tr key={item._id}>
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                                                <Link to={`/productDetail/${item._id}`} state={item}>
-                                                    <img src={item.productImages[0]} className="w-16 cursor-pointer h-16 rounded-xl" alt="" /></Link>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                                                {item.price} Tk
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                                                {item.productName}
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium text-gray-800 dark:text-gray-200">
-                                                {item.availableSize.map((size, index) => (
-                                                    <span key={index} className="mr-2 p-[2px] rounded-md bg-gray-200">{size}</span>
-                                                ))}
-                                            </td>
+        <div className="h-full w-full flex flex-col justify-center ">
+            <div className="flex flex-col mb-10">
+                <h1 className="text-3xl font-semibold text-center mb-10 ml-14">
+                    Manage All The Product
+                </h1>
+                <div className="flex flex-col lg:container max-w-[98vw] mx-auto">
+                    <div className="overflow-x-auto border-2 rounded-xl">
+                        <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                            <thead>
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                                    >
+                                        Product Image
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                                    >
+                                        Product Price
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                                    >
+                                        Product Title
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                                    >
+                                        AvailAble Size
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                                    >
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                                {allProduct?.map((item) => (
+                                    <tr key={item._id}>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                                            <Link to={`/productDetail/${item._id}`} state={item}>
+                                                <img src={item.productImages[0]} className="w-16 cursor-pointer h-16 rounded-xl" alt="" /></Link>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                                            {item.price} Tk
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                                            {item.productName}
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium text-gray-800 dark:text-gray-200">
+                                            {item.availableSize.map((size, index) => (
+                                                <span key={index} className="mr-2 p-[2px] rounded-md bg-gray-200">{size}</span>
+                                            ))}
+                                        </td>
 
-                                            <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200 flex space-x-2">
-                                                <Link state={item} to={`/dashboard/editProduct/${item._id}`} className="p-2 rounded-full bg-green-700 text-white hover:bg-green-700 border-none"><FiEdit size={20} /> </Link>
-                                                <button onClick={() => handleDeleteProduct(item._id)} className="p-2 rounded-full bg-red-500 text-white hover:bg-red-500 border-none"><MdDeleteOutline size={20} /> </button>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200 flex space-x-2">
+                                            <Link state={item} to={`/dashboard/editProduct/${item._id}`} className="p-2 rounded-full bg-green-700 text-white hover:bg-green-700 border-none"><FiEdit size={20} /> </Link>
+                                            <button onClick={() => handleDeleteProduct(item._id)} className="p-2 rounded-full bg-red-500 text-white hover:bg-red-500 border-none"><MdDeleteOutline size={20} /> </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
